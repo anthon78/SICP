25e8d9926fa67145fa82c25886fef6eb1deca699
anthon78 <economicsstudent2000@gmail.com>
2018-02-08 23:19:35 -0500

    complete ex 3

---
+++ Ch1/ex1-3.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (square-larger a b c)
+  (cond [(and (> a c) (> b c)) (+ (* a a)(* b b))]
+        [(and (> a b) (> c b)) (+ (* a a)(* c c))]
+        [else (+ (* b b)(* c c))]))
+
+
+        
+  
